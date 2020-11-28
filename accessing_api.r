9d3ccf817cbb67165041d9c8d41c466bc52d2cdc
harryob2 <60663887+harryob2@users.noreply.github.com>
2020-11-28 01:03:24 +0000

    Create accessing_api.r
    just set it up

---
+++ accessing_api.r
@@ -0,0 +1,8 @@
+# https://www.programmableweb.com/news/how-to-access-any-restful-api-using-r-language/how-to/2017/07/21
+
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
